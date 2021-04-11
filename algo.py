108c9a4b8abf9c07519a33c651b738a82793e613
Michael Batrakov <michaelbatrakov@gmail.com>
2021-04-10 21:27:46 -0700

    first commit

---
+++ algo.py
@@ -0,0 +1,88 @@
+from pyxdameraulevenshtein import damerau_levenshtein_distance
+import csv
+
+_first = []
+second = []
+third = []
+fourth = []
+fifth = []
+sixth = []
+seventh = []
+eigth = []
+ninth = []
+tenth = []
+
+columns = []
+
+diff_tmp = 0.0
+diff_closest = 0.0
+
+with open('test.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+
+    # Extracts columns out of the rows
+    for row in spamreader:
+        transformed_row = row[0].split(',')
+        # print(transformed_row)
+        # first.append(transformed_row[0])
+        second.append(transformed_row[1])
+        third.append(transformed_row[2])
+        fourth.append(transformed_row[3])
+        fifth.append(transformed_row[4])
+        sixth.append(transformed_row[5])
+        seventh.append(transformed_row[6])
+        eigth.append(transformed_row[7])
+        ninth.append(transformed_row[8])
+        tenth.append(transformed_row[9])
+
+        # print(row, '||||')
+
+# print(second)
+second.pop(0)
+third.pop(0)
+fourth.pop(0)
+fifth.pop(0)
+sixth.pop(0)
+seventh.pop(0)
+eigth.pop(0)
+ninth.pop(0)
+tenth.pop(0)
+
+
+columns.append(','.join(second))
+columns.append(','.join(third))
+columns.append(','.join(fourth))
+columns.append(','.join(fifth))
+columns.append(','.join(sixth))
+columns.append(','.join(seventh))
+columns.append(','.join(eigth))
+columns.append(','.join(ninth))
+columns.append(','.join(tenth))
+
+# * Compares adjacent Columns
+# for column_index in range(8):
+#     print(columns[column_index])
+#     print(columns[column_index+1])
+
+#     diff_tmp += damerau_levenshtein_distance(columns[column_index].split(','), columns[column_index + 1].split(',')) 
+
+for column_index in range(8):
+    # * First Column
+    diff_tmp += damerau_levenshtein_distance(columns[0].split(','), columns[column_index + 1].split(',')) 
+    # * Second Column
+    print(column_index)
+    if column_index < 7:
+        diff_tmp += damerau_levenshtein_distance(columns[1].split(','), columns[column_index + 2].split(','))
+    if column_index < 6:
+        diff_tmp += damerau_levenshtein_distance(columns[2].split(','), columns[column_index + 3].split(',')) 
+ 
+
+
+
+# * Results
+
+diff_closest += (diff_tmp / 9)
+print('diff temp: ', diff_tmp)
+print('diff closest', diff_closest)
+
+# print(damerau_levenshtein_distance([1, 2, 3, 4, 5, 6], [7, 8, 9, 7, 10, 11, 4]))
