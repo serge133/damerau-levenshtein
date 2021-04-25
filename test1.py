67982ab268dd5bb40fecd68bd722f42321f2907f
Michael Batrakov <michaelbatrakov@gmail.com>
2021-04-25 16:07:41 -0700

    pcolor graph works, bugs with algo

--- test1.py
+++ test1.py
@@ -10,37 +10,36 @@ website2 = 0.1
 
 
 websites_to_values = []
-data = []
 
 websites = []
 
+graph_data = []
+
 with open('immigration_incognito.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-
     # Extracts columns out of the rows
     for row in spamreader:
         transformed_row = row[0].split(',')
+        print(transformed_row)
         websites.append(transformed_row[0])
-        # second.append(transformed_row[1])
-        # third.append(transformed_row[2])
-        # fourth.append(transformed_row[3])
-        # fifth.append(transformed_row[4])
-        # sixth.append(transformed_row[5])
-        # seventh.append(transformed_row[6])
-        # eigth.append(transformed_row[7])
-        # ninth.append(transformed_row[8])
-        # tenth.append(transformed_row[9])
+        graph_data_row = []
+        transformed_row.pop(0)
 
+        for num in transformed_row:
+            graph_data_row.append(float(num) / 10)
+
+        graph_data.append(graph_data_row)
         # print(row, '||||')
 websites.pop(0)
+
 
 num = 0
 for website in websites:
     websites_to_values.append(num)
     num += 0.1
 
-data.append(websites_to_values)
-graph = plt.pcolor(data )
+print(websites, websites_to_values)
+graph = plt.pcolor(graph_data)
 plt.title('Graph')
 plt.ylabel("Search Results")
 plt.xlabel('Participants')
